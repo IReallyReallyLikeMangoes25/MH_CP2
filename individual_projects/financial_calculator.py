394191b609f7628fb0a0539c5cb799fbc46aebff
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-01-08 09:51:54 -0700

    working on financial calculator

---
+++ individual_projects/financial_calculator.py
@@ -0,0 +1,11 @@
+# MH 1st financial calculator project
+
+# function for saving to a goal:
+    
+# function for compound interest calculator:
+
+# function for budget allocator:
+
+# function for discount calculator
+
+# function for tip calculator:
