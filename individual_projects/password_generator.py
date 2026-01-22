b915985a9b9e9a97614b59308fc0d77f2c7c9e36
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-01-22 09:51:20 -0700

    working on  a password generator

---
+++ individual_projects/password_generator.py
@@ -0,0 +1,39 @@
+# MH 1st pasword generator project
+
+# import random
+
+# list for all letters a-z
+# list for numbers 0-9
+# list for special characters
+
+# prefferences function:
+    # ask user preffered length
+    # ask if capital letters should be used
+    # ask if lowercase letters should be used
+    # ask if numbers should be used
+    # ask if special characters should be used
+    # return all prefferences
+
+# generation function, takes in prefferences:
+    # loop that runs four times
+        # loop that runs until password is preffered length
+            # generate random number 1-3
+            # if the number is one add a random letter
+            # if the number is two add a random number
+            # if the number is three add a random special character
+        # save generated password
+    # if capitals should be used:
+
+    # if lowercase shouldn't be used:
+
+    # if numbers shouldn't be used:
+
+    # if special characters shouln't be used:
+    
+
+# main function:
+    # loop that runs until they want to exit
+    # ask if they would like to generate passwords
+    # if they do:
+    # run prefferences function
+    # run generation function on their prefferences and print results
