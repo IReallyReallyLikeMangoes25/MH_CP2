4114ada6580340bf8fe68f31d084f9999fdc2ea6
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-02-17 23:48:38 -0700

    fixing this

--- individual_projects/update_personal_library.py
+++ individual_projects/update_personal_library.py
@@ -1,9 +1,34 @@
 # Mh first update personal library program
 
-# add full display function
+# add full display function, takes in collection dictionary
+def full_display(rocks):
+    if len(rocks) <= 0:
+        print("There are no rocks or minerals in your collection.")
+    else:
+        # loops over dictionary printing every item and all its details
+        for key, value in rocks.items():
+            print(f"{key[0]} : {value[0]}, {key[1]} : {value[1]}, {key[2]} : {value[2]}, {key[3]} : {value[3]}")
+
 # add update function, takes in the collection dictionary
-def update(rock_collection):
-    # asks user 
+def update(rocks):
+    # prints full list with numbers
+     if len(rocks) <= 0:
+        print("There are no rocks or minerals in your collection.")
+    else:
+        # loops over dictionary printing every item and all its details
+        for i, key, value in rocks.items():
+            print(f"{i}.{key[0]} : {value[0]}, {key[1]} : {value[1]}, {key[2]} : {value[2]}, {key[3]} : {value[3]}")
+    # asks user which number they would like to edit
+    to_update = input("what number do you want to update?\n") 
+    to_update = int(to_update)
+    # asks what part they want to edit
+    section = input("What part do you want to update? 1. Name, 2. Category, 3. Crystal System, or 4. Formula\n")
+    section = int(section)
+    # changes that part based off their input
+    new = input("What would you like to change it to?\n")
+    rocks[to_update[section]] = new
+    # reaturns updated dictionary
+    return rocks
 
 # add load csv function
 def load_csv():
@@ -17,17 +42,20 @@ def load_csv():
         return rows
 
 # add save changes function that takes in the rock collection dictionary
-def save_changes(rock_collection):
+def save_changes(rocks):
     # loops over csv and dictionary changing every line of the csv to be the same as in the dictionary.
+    with open("individual_projects/collection.csv", "w") as collection:
+        writer = csv.writer(collection)
+        writer.writeRows(rocks)
 
 # view function, takes in dictionary
-def view_collection(rocks):
+def simple_display(rocks):
     if len(rocks) <= 0:
         print("There are currently no rocks or minerals in your collection.")
     else:
         # loops over dictionary, priting key value pairs one by one
         for key, value in rocks.items():
-            print(f"{key}:{value}")
+            print(f"{key[0]}:{value[0]}, {key[1]}, {value[1]}")
 
 # remove function, takes in dictionary
 def remove(rocks):
@@ -58,70 +86,30 @@ def add():
     # returns rock and type
     return name, type
 
-# search function, takes in dictionary
-def search(rocks):
-    in_collection = False
-    # asks user what they want to search for (rock/mineral name or rock/mineral type)
-    search = input("What would you like to search for:\n1. Rock/Mineral name\n2. Rock/Mineral type\n")
-    while True:
-        # if they want to search by name, ask them for the rock/mineral name
-        if search == "1":
-            name = input("What is the rock/mineral you are searching for?\n")
-            # if it's in the dictionary print out it's key value pair
-            for key, value in rocks.items():
-                if key == name:
-                    print(f"{key}, {value}")
-                    in_collection = True
-            # if it's not in the dictionary display that it is not in the dictionary
-            if in_collection == False:
-                print("That is not in your collection.")
-            break
-        # if they want to search for type, as them for a rock?mineral type
-        elif search == "2":
-            type_of_rock = input("What type of rock/mineral are you searching for?\n")
-            # if it's in the dictionary print out it's key value pair
-            for key, value in rocks.items():
-                if value == type_of_rock:
-                    print(f"{key}, {value}\n")
-                    in_collection = True
-            # if it's not in the dictionary display that it is not in the dictionary
-            if in_collection == False:
-                print("That is not in your collection.")
-            break
-        else:
-            print("That is not an option.")
-            search = input("What would you like to search for:\n1. Rock/Mineral name\n2. Rock/Mineral type")
-
 # menu function
 def menu():
     while True:
-        # tuple containing strings that say the options of what to do
-        options = ("1. View your collection","2. Add to your collection", "3. Remove from your collection", "4. Search your collection", "5. Exit")
-        # print the tuple
-        for i in options:
-            print(i)
-
-    # ask user what they would like to do from the given options
-        choice = input("Which number would you like to do?\n")
-    # if they want to view run the view function
-        if choice == "1":
-            view_collection(collection)
-    # if they want to add run the add function
-        elif choice == "2":
-            new_name, new_type = add()
-            collection[new_name] = new_type
-            print("Added")
-    # if they want to remove run the remove function
-        elif choice == "3":
-            delete = remove(collection)
-            del collection[delete]
-            print("Removed")
-    # if they want to search run the search function
-        elif choice == "4":
-            search(collection)
-        elif choice == "5":
-            break
-        else:
+        rock_collection = load_csv()
+        save_changes(rock_collection)
+        # asks what the user wants to do
+        action = input("What do you want to do? 1. View simple list, 2. View detailed list, 3. Update an item on the list, 4. Remove something from the list, or 5. Add something to the list, 6. Exit\n")
+        # if they want to view the simple version run the simple view function
+        if action == "1": 
+            simple_display(rock_collection)
             continue
-
-menu()
+        # if they want to view the full version run the full view function
+        elif action == "2":
+            full_display(rock_collection)
+            continue
+        # if they want to update something run the update function
+        elif action == "3":
+            rock_collection = update(rock_collection)
+            continue
+        # if they want to add run the add function
+        elif action == "4":
+            rock_collection = add(rock_collection)
+            continue
+        # if they want to remove run the remove function
+        elif action == "5":
+            rock_collection = remove(rock_collection)
+        # if they want to exit exit the code
