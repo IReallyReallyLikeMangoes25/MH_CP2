b63a0e1f9d6f3b64484ac9fde37fa9296ffbbc1b
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-02-23 09:49:59 -0700

    holy

--- individual_projects/word_counter_main.py
+++ individual_projects/word_counter_main.py
@@ -5,21 +5,22 @@ from word_counter_mangement import *
 from word_counter_time import *
 
 last_updated = "Not yet updated."
-word_count = 0
+word_count = count_words()
 
 # menu function:
 def main(updated, words):
     print("Welcome to Mirai's word counter program. In this program you have a file you may edit, view, or check the information of.")
     # automatically sets last updated and word count to 0
     # asks user if they want to update file, view file, view doccument info, or exit
+    file = get_file()
     while True:
             action = input("\nWhat do you want to do?\n1. Update doccument\n2. View doccument\n3. View doccument info\n4. Exit\nChoice (1-4): ")
             # if they want to update run the update function and save the last updated and word count
             if action == "1":
-                updated, words = update_file(words)
+                updated, words = update_file(words, file)
             # if they want to view the file run the view file function
             elif action == "2":
-                view_file(word_count)
+                view_file(words, file)
             # if they want to view the doccument info print whatever is saved as last updated and word count
             elif action == "3":
                 print(f"\nLast updated : {updated}\nWord Count : {words}")
--- individual_projects/word_counter_mangement.py
+++ individual_projects/word_counter_mangement.py
@@ -2,31 +2,46 @@
 
 # import from other files
 from word_counter_time import *
+from pathlib import Path
 
-# runs word count function
+# get file function:
+def get_file():
+    while True:
+        # asks user for the exact relative path of the file they want to update (must be txt)
+        file = input("Please input the exact relative path of the file you would like to open (must be txt):\n")
+        # checks if it's a txt file
+        file_path = Path(file)
+        file_type = file_path.suffix
+        # if it's not ask again
+        if file_type != ".txt":
+            print("That is not a txt file.")
+            continue
+        # if it is continue
+        else: break
+    # returns relative path
+    return file_path
 
 # update file function:
-def update_file(words):
+def update_file(words, relative_path):
     # asks user what they want to add to the file
     to_add = input("\nType below hat you would like to add:\n")
-    with open("individual_projects/word_counter_file.txt", "a") as file:
-        file.write(to_add)
-    to_add = to_add.split()
+    with open(relative_path, "a") as file:
+        file.write(f"\n{to_add}")
     # uses save time function to get the time updated
     time_updated = save_time()
     # runs word count function
-    words += count_words(len(to_add))
+    words = count_words(to_add, relative_path)
     # adds new text to the file
     # adds time updated and word count
-    with open("individual_projects/word_counter_file.txt", "a") as file:
+    with open(relative_path, "a") as file:
         file.write(f"\n\nUpdated : {time_updated}\nWord count : {words}")
     # returns word count, time updated, and what they added for use later
     return time_updated, words
 
 # view file function:
-def view_file(words):
+def view_file(words, relative_path):
     # opens the file and prints it, if it's empty it prints it is empty
-    with open("individual_projects/word_counter_file.txt", "r") as file:
+    with open(relative_path, "r") as file:
         if int(words) == 0:
             print("\nThis file is empty.")
         else:
@@ -34,9 +49,11 @@ def view_file(words):
             print(content)
 
 # word count function:
-def count_words(added):
-    # loops over what was added to the file separating words at spaces and counting how many there are
-    added = added.split()
-    count = len(added)
-    # returns the amoutn of words
-    return str(count)
+def count_words(relative_path):
+    # loops over the file counting every word
+    with open(relative_path, "r") as file:
+        content = file.read()
+        words = content.split()
+        count = len(words)
+    # returns the amount of words
+    return count
