1a77f8edd1c5a787dfa62b8eb8df709a5bc3ab25
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-02-26 09:51:25 -0700

    working on fractal generator

---
+++ individual_projects/fractal_generator.py
@@ -0,0 +1,29 @@
+# MH 1st fractal generator program
+
+# import turtle
+
+# function to draw triangle, depth, color, base location, base side length, and base times to repeat
+    # draws three base triangles
+    # for loop:
+        # if the depth is 0 then exit the function
+        # otherwise:
+            # for loop that loops as many times as to repeat
+                # moves locations to the beginning of the first triangle
+                # draws 3 triangles
+            # sets new triangle side length
+            # sets new beginning location
+            # subtracts one from depth
+            # multiplies times to repeat by three
+            # runs the function again, passing through depth, color, new location, new side length, and times to repeat
+
+# draw three triangles function, takes in side length and color:
+    # draws large triangle with 2X desired side length
+    # draws smaller triangle inside with normal side length upsidown
+
+
+
+
+# menu function
+    # asks user the desired depth
+    # asks user the desired color
+    # runs generation function, passing through chosen depth and color
