4d2c75ecba7437d4a1529985de7d25ff5c5a7af5
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-03-03 01:32:22 -0700

    Update position_tracker.py

--- individual_projects/position_tracker.py
+++ individual_projects/position_tracker.py
@@ -1,1 +1,31 @@
-# nothing yet
+# MH 1st position tracking/setting file
+
+# position functions, take in side length and move turtle to beginning position of next triangle
+def position_1_draw(side_length, turtle):
+    turtle.penup()
+    turtle.fd(side_length)
+    turtle.pendown()
+def position_2_draw(side_length, turtle):
+    turtle.penup()
+    turtle.right(120)
+    turtle.fd(side_length)
+    turtle.pendown()
+def final_position_draw(side_length, turtle):
+    turtle.penup()
+    turtle.fd(side_length)
+    turtle.left(60)
+    turtle.fd(side_length)
+    turtle.pendown()
+
+def position_2_recurse(side_length, turtle):
+    turtle.penup()
+    turtle.right(180)
+    turtle.fd(side_length)
+    turtle.right(60)
+    turtle.fd(side_length)
+    turtle.pendown()
+def final_position_recurse(side_length, turtle):
+    turtle.penup()
+    turtle.seth(0)
+    turtle.fd(side_length)
+    turtle.pendown()
