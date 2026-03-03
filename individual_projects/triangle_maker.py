db16d64cd274f32216476945f0e9cab36fc71bab
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-03-03 01:31:30 -0700

    Update triangle_maker.py

--- individual_projects/triangle_maker.py
+++ individual_projects/triangle_maker.py
@@ -1,1 +1,14 @@
-# nothing yet
+# MH 1st generate triangles file
+
+# draw three triangles function, takes in side length and color:
+def draw_triangle(side_length, color, turtle):
+    turtle.seth(0)
+    turtle.right(60)
+    turtle.color(color)
+    # draws a triangle
+    turtle.fd(side_length)
+    turtle.right(120)
+    turtle.fd(side_length)
+    turtle.right(120)
+    turtle.fd(side_length)
+    turtle.right(120)
