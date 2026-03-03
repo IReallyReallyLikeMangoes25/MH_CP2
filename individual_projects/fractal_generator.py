5766eb8cfbf126332eba3d0771bbee9277664272
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-03-03 01:30:46 -0700

    Update fractal_generator.py

--- individual_projects/fractal_generator.py
+++ individual_projects/fractal_generator.py
@@ -1,97 +1,30 @@
-# MH 1st fractal generator program
-#x = 0
-#y = 200
+# MH 1st fractal generation file
 
-# import turtle
-import turtle
+from positions import *
+from draw_triangle import *
 
-# function to draw triangle, depth, color, base location, base side length, and base times to repeat
-def draw_fractal(depth, color, x, y, side_length):
-    turtle.up()
-    turtle.goto(x, y)
-    repeat = 3
-    # draws three base triangles
-    draw_triangles(side_length, color)
-    # if the depth is 0 then exit the function
-    if depth == 0:
-        return
-    # otherwise:
+# function to draw fractal, takes in depth, color, and side length
+def draw_fractal(color, side_length, current_level, desired_level, turtle):
+    # if the level is not the smallest level, go a level inward and check again by calling this function
+    if current_level < desired_level:
+        draw_fractal(color, side_length/2, current_level+1, desired_level, turtle)
+        # draw more triangles
+        draw_fractal(color, side_length/2, current_level+1, desired_level, turtle)
+        # go to next position
+        position_2_recurse(side_length, turtle)
+        # draw more triangles
+        draw_fractal(color, side_length/2, current_level+1, desired_level, turtle)
+        # go to next position
+        final_position_recurse(side_length, turtle)
+    # if the level is the smallest level draw many of the smallest triangle needed
     else:
-        # for loop that loops as many times as to repeat
-        for triangle in range(repeat):
-            # draws 3 triangles
-            draw_triangles(side_length, color)
-            # moves locations to the beginning of the next triangle
-            turtle.fd(side_length)
-        # sets new triangle side length
-        # sets new beginning location
-        # subtracts one from depth
-        # multiplies times to repeat by three
-        # runs the function again, passing through depth, color, new location, new side length, and times to repeat
-
-# draw three triangles function, takes in side length and color:
-def draw_triangles(side_length, color):
-    turtle.color(color)
-    turtle.right(180)
-    # draws large triangle with 2X side length
-    turtle.down()
-    turtle.fd(side_length)
-    turtle.right(120)
-    turtle.fd(side_length * 2)
-    turtle.right(120)
-    turtle.fd(side_length * 2)
-    turtle.right(120)
-    turtle.fd(side_length)
-    # draws smaller triangle inside with normal side length upsidown
-    turtle.left(180)
-    turtle.fd(side_length)
-    turtle.left(120)
-    turtle.fd(side_length)
-    turtle.left(120)
-    turtle.fd(side_length)
-    turtle.right(120)
-    turtle.fd(side_length)
-    turtle.left(240)
-    turtle.fd(side_length)
-    turtle.right(120)
-    turtle.fd(side_length)
-    turtle.left(120)
-    turtle.fd(side_length/2)
-
-
-# menu function
-    # asks user the desired depth
-    # asks user the desired color
-    # runs generation function, passing through chosen depth and color
-turtle.speed(10)
-draw_triangles(200, 'blue')
-draw_triangles(100, "blue")
-turtle.up()
-turtle.right(180)
-turtle.fd(250)
-turtle.right(180)
-draw_triangles(100, "blue")
-turtle.right(180)
-turtle.fd(150)
-turtle.right(120)
-turtle.fd(200)
-turtle.right(60)
-turtle.fd(100)
-draw_triangles(100, "blue")
-draw_triangles(50, "blue")
-turtle.up()
-turtle.right(180)
-turtle.fd(125)
-turtle.right(180)
-draw_triangles(50, "blue")
-turtle.right(180)
-turtle.fd(75)
-turtle.right(120)
-turtle.fd(100)
-turtle.right(60)
-turtle.fd(50)
-draw_triangles(50, "blue")
-turtle.right(180)
-turtle.fd(50+25)
-turtle.left(60)
-turtle.done()
+        # actually draw the triangles now that you are in the smallest level
+        draw_triangle(side_length, color, turtle)
+        # go to next position
+        position_1_draw(side_length, turtle)
+        draw_triangle(side_length, color, turtle)
+        # go to next position
+        position_2_draw(side_length, turtle)
+        draw_triangle(side_length, color, turtle)
+        # go to next position
+        final_position_draw(side_length, turtle)
