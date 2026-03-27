dd370a3fbc94e10410df52b727477d4f47c030bd
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-03-27 09:51:48 -0600

    yay not super crazy project huzzah

---
+++ simple_grade_book/gradebook_class.py
@@ -0,0 +1,22 @@
+# mh 1st gradebook class
+
+class gradebook:
+    def __init__(self, students):
+        self.students = students
+        
+    # search by grade, takes in students:
+        # asks what letter grade they are searching for
+        # loops through all students and if a student has that letter grade add them to a list
+        # print out the list
+
+    # search by name, takes in students:
+        # asks what name they are searching for
+        # loops through all students and if a student has that name add them to a list
+        # print out the list
+
+    # view student record function:
+        # uses search function to find student they want to vie wthe record of
+        # prints students name, id, average grade, letter grade, grade entries, standing, and year
+
+    # view all students function, takes in students:
+        # prints every student and their id, average grade, anbd letter grade
--- simple_grade_book/main.py
+++ simple_grade_book/main.py
@@ -2,8 +2,6 @@
 
 # to do list:
 # function to add student
-# function to add grade
 # search by id function
 # search by name function
-# average grade calculator
-# letter grade calculator
+# menu
