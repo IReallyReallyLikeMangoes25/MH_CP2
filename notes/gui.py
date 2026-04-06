8004a0f36ffabd7df95e30d40286522e8522835c
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2026-04-06 09:51:42 -0600

    notes yay

---
+++ notes/gui.py
@@ -0,0 +1,28 @@
+# MH 1st gui notes
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Testing")
+root.configure(background = "orange")
+root.minsize(250, 250)
+root.maxsize(1000, 1000)
+# dimensions followed by x and y
+root.geometry("300x300+100+100")
+label = tk.Label(root, text = "This is currently working!", font = ("Times New Roman", 14, "bold"))
+label.config(fg = "blue", background = "green")
+label.pack()
+image = tk.PhotoImage(file = "notes/images/falling-bread-bread.gif")
+tk.Label(root, image = image).pack()
+# stuff about buttons
+root.count = 0
+def add():
+    root.count += 1
+    tk.Label(root, text = root.count).pack()
+    
+btn = tk.Button(root, text = "Add", command = add)
+btn.pack()
+
+label.pack()
+
+root.mainloop()
